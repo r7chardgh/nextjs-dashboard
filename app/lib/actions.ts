0386384ff80343148a0b41d3afdf1ad85ce2f822
richard tsang <richard.tsang9641@gmail.com>
2024-04-16 15:59:09 +0800

    Complete chapter 13 - error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   console.log(date);
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId},${amountInCents},${status},${date})
-  `
+
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId},${amountInCents},${status},${date})
+    `
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
 
   revalidatePath('/dashboard/invoices');  //by default nextjs has a client-side router cache, this function can clear old cache and revalidate it
   redirect('/dashboard/invoices'); //redirect to the path
@@ -49,11 +56,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
   UPDATE invoices
   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
   WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice'
+    }
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices')
@@ -62,8 +75,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 //delete invoice by id
 
 export async function deleteInvoice(id: string,) {
-  await sql`
-  DELETE FROM invoices WHERE id = ${id}
-  `
-  revalidatePath('/dashboard/invoices');
+
+  //throw new Error("Failed to Delete Invoice");
+  try {
+
+    await sql`
+    DELETE FROM invoices WHERE id = ${id}
+    `
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return {
+      message: "Database Error : Failed to Delete Invoice"
+    }
+  }
 }
